7e14def68bed893012fdab8ff216073317955049
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2026-06-08 17:46:29 -0600

    feat: implementar pantalla calculadora con Jetpack Compose

--- app/src/main/java/ni/edu/uam/calculadoracomposetest/MainActivity.kt
+++ app/src/main/java/ni/edu/uam/calculadoracomposetest/MainActivity.kt
@@ -3,45 +3,46 @@ package ni.edu.uam.calculadoracomposetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ni.edu.uam.calculadoracomposetest.ui.theme.CalculadoraComposeTestTheme
+import androidx.compose.ui.platform.testTag
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            CalculadoraComposeTestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PantallaCalculadora()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun PantallaCalculadora() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculadoraComposeTestTheme {
-        Greeting("Android")
+    val calculadora = Calculadora()
+
+    var resultado by remember {
+        mutableStateOf("")
+    }
+
+    Column {
+
+        Button(
+            onClick = {
+                resultado = calculadora.sumar(5, 3).toString()
+            }
+        ) {
+            Text("Calcular")
+        }
+
+        Text(
+            text = resultado,
+            modifier = Modifier.testTag("resultado")
+        )
     }
 }
